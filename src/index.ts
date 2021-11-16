583b7074c9bbb81dd262001777508bed30919990
akrck02 <aketza.vazquez@gmail.com>
2021-11-16 13:51:45 +0100

    [Setup] Port set up to 80

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { API_URL, Router } from "./router";
 
-const PORT = 3000;
+const PORT = 80;
 const app = express();
 const router = new Router();
 router.declare();
